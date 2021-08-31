e60593b4c0cf17bc2ab7100b896b95911dc9f789
Artsiom Burdukou <artsiomburdukou@gmail.com>
2021-08-31 13:21:02 +0300

    feat: add implementation GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.candidate = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max
     }
 
     guess() {
+        this.candidate = this.min + Math.round((this.max - this.min) / 2);
 
+        return this.candidate;
     }
 
     lower() {
-
+        this.setRange(this.min, this.candidate);
     }
 
     greater() {
-
+        this.setRange(this.candidate, this.max);
     }
 }
 
